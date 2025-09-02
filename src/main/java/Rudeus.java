286008eded131dc5e6c7f5190dc35db732a80196
Wrooi <ooiwenree@gmail.com>
2025-09-02 12:42:21 +0800

    Update Rudeus to echo user input & exit after 'bye' command

--- src/main/java/Rudeus.java
+++ src/main/java/Rudeus.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Rudeus {
     public static void main(String[] args) {
         String logo =
@@ -11,8 +13,21 @@ public class Rudeus {
                         "│ ░██     ░██   ░██████   ░███████   ░██████████   ░██████     ░██████   │\n" +
                         "└────────────────────────────────────────────────────────────────────────┘\n";
         System.out.println("Yo! The name's \n" + logo + "At your service, as always. Need some magic—or maybe just a hand? Ask away!\n" +
-                "──────────────────────────────────────────────────────────────────────────");
-        System.out.println(" See you around! Don’t get into too much trouble without me.\n" +
+                "──────────────────────────────────────────────────────────────────────────\n");
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        do{
+            userInput = scanner.nextLine().trim();
+            System.out.print("──────────────────────────────────────────────────────────────────────────\n");
+            if(userInput.equalsIgnoreCase("bye")){
+                break;
+            }
+            System.out.println(userInput);
+            System.out.println("──────────────────────────────────────────────────────────────────────────\n");
+        } while (true);
+        scanner.close();
+
+        System.out.println("See you around! Don’t get into too much trouble without me!\n" +
                 "──────────────────────────────────────────────────────────────────────────");
     }
 }
